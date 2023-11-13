c7e37452ca5b5daec919b534dc61861bb23d5d3e
vincentcheri <vj9783@gmail.com>
2023-11-13 12:27:04 -0800

    header file for printf project

---
+++ main.h
@@ -0,0 +1,36 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <stdlib.h>
+
+/**
+ * struct format_spec - matches format specifier with function pointer
+ * @str: specifying character
+ * @func: function pointer that matches specifier
+ */
+
+typedef struct format_spec
+{
+	char *str;
+	int (*func)(va_list ap);
+} spec;
+
+/* declaration of _printf function */
+int _printf(const char *format, ...);
+
+/* declaration of _putchar function */
+int _putchar(char c);
+
+/* declaration of print_char function */
+int print_char(va_list ap);
+
+/* declaration of print_string function */
+int print_string(va_list ap);
+
+/* declaration of print_perc function */
+int print_perc(va_list ap);
+
+
+
+#endif /* MAIN_H */
