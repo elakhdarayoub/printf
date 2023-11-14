d4ccc7863daa888cd6685b51f5087f325867404b
vincentcheri <52957917+vincentcheri@users.noreply.github.com>
2023-11-14 13:18:46 +0100

    Update functions.c

--- functions.c
+++ functions.c
@@ -37,7 +37,7 @@ int print_str(va_list ap)
 
 	/* calling recursivly our _print to output null if str is NULL */
 	if (!str)
-		return (_printf("(null)"));
+		return ("(null)");
 
 
 	for (i = 0; str[i] != '\0'; i++)
