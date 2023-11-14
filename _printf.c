6eb003ccd9be6ad68d3bd03a607d301477b1d40a
vincentcheri <vj9783@gmail.com>
2023-11-14 16:34:11 +0100

    -

--- _printf.c
+++ _printf.c
@@ -22,7 +22,7 @@ int _printf(const char *format, ...)
 	for (i = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] != '%')
-			_putchar(format[i]);
+			count +=_putchar(format[i]);
 		/* we jump in here because format is a % */
 		else
 		{
