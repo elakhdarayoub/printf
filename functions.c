6ab8124dc70bb6e1094a779c9d1f7f707c53271d
vincentcheri <52957917+vincentcheri@users.noreply.github.com>
2023-11-14 13:07:42 +0100

    Update functions.c

--- functions.c
+++ functions.c
@@ -32,19 +32,20 @@ int print_char(va_list ap)
 
 int print_str(va_list ap)
 {
-	char *x = va_arg(ap, char*), *nil_string = "(null)";
-	int y, len, len2 = string_length(nil_string);
+	char *str = va_arg(ap, char *);
+	int i;
 
-	if (x == NULL)
+	/* calling recursivly our _print to output null if str is NULL */
+	if (!str)
 	{
-		for (y = 0; nil_string[y]; y++)
-			_putchar(nil_string[y]);
-		return (len2 - 1);
+		_printf("(null)");
+		return (-1);
 	}
-	len = string_length(x);
-	for (y = 0; y < len ; y++)
-		_putchar(x[y]);
-	return (len - 1);
+
+
+	for (i = 0; str[i] != '\0'; i++)
+		_putchar(str[i]);
+	return (i);
 }
 
 /**
