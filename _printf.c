d54bcc1f40d28e24d10e3035075a1c3b07dea2ca
vincentcheri <52957917+vincentcheri@users.noreply.github.com>
2023-11-15 12:27:54 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -11,11 +11,10 @@ int _printf(const char *format, ...)
 	map_t ops[] = {
 		{'c', print_char}, {'s', print_str}, {'\0', NULL}};
 
-	int i, j, count = 0;
+	int i, j, count = 0, spec = 0;
 	va_list ap;
 	int specf;
-
-	specf = 0;
+	
 	va_start(ap, format);
 	/* If The format passed is NULL in any form just exit with -1 */
 	if (!format || strcmp(format, "%") == 0)
@@ -24,40 +23,32 @@ int _printf(const char *format, ...)
 	for (i = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] != '%')
-		{
 			count += _putchar(format[i]);
-		}
 
 		/* we jump in here because format is a % */
 		else
 		{
 			if (format[i + 1] == '%')
-			{
 				_putchar('%');
 				count++;
 				i++;
-			}
 
 			else
 			{
 				for (j = 0; ops[j].spec != '\0'; j++)
 				{
 					if (format[i + 1] == ops[j].spec)
-					{
 						count += ops[j].func(ap);
 						i++;
 						specf = 1;
 						break;
-					}
 				}
 
 				if (!specf)
-				{
 					_putchar('%');
 					_putchar(format[i + 1]);
 					count += 2;
 					i++;
-				}
 			}
 		}
 	}
