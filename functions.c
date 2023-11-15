98f2edc922ed0d3e52cca464a0cea916fbe3731c
Ayoub Elakhdar <raja500.ayoub@gmail.com>
2023-11-15 20:05:00 +0100

    getting back

--- functions.c
+++ functions.c
@@ -5,18 +5,15 @@
  * @c: The char to print.
  * Return: 1.
  */
-
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
-
 /**
  * print_char - Prints the %c specifier.
  * @ap: char to be printed
  * Return: 1.
  */
-
 int print_char(va_list ap)
 {
 	int c = va_arg(ap, int);
@@ -29,7 +26,6 @@ int print_char(va_list ap)
  * @ap: The string to print.
  * Return: The number of chars printed.
  */
-
 int print_str(va_list ap)
 {
 	char *str = va_arg(ap, char *);
@@ -43,11 +39,8 @@ int print_str(va_list ap)
 	else
 	{
 		for (i = 0; str[i] != '\0'; i++)
-			{
 				_putchar(str[i]);
-			}
 	}
-
 	return (i);
 }
 
@@ -55,7 +48,6 @@ int print_str(va_list ap)
  * print_mod - Prints the modulus char.
  * Return: printed %
  */
-
 int print_mod(void)
 {
 	return (_putchar('%'));
