04bbfab84d147d55791b9c624108b68e9b00c774
vincentcheri <52957917+vincentcheri@users.noreply.github.com>
2023-11-17 16:29:44 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,62 +1,59 @@
 #include "main.h"
+#include <string.h>
 
 /**
  * _printf - A light-weight implementation of standard printf function.
  * @format: Pointer to a constant string.
- * Return: Number of characters been printed.
+ * Return: Number of characters printed (excluding the null byte used to end output to strings).
  */
 int _printf(const char *format, ...)
 {
-	map_t ops[] = {
-		{'c', print_char}, {'s', print_str}, {'\0', NULL}};
+    map_t ops[] = {
+        {'c', print_char}, {'s', print_str}, {'%', print_mod}, {'\0', NULL}};
 
-	int i, j, count = 0;
-	va_list ap;
-	int specf;
+    int i, j, count = 0, specf = 0;
+    va_list ap;
 
-	specf = 0;
-	va_start(ap, format);
-	/* If The format passed is NULL in any form just exit with -1 */
-	if (!format || strcmp(format, "%") == 0)
-		return (-1);
-	/* The main loop that replaces specifiers with their corrs args */
-	for (i = 0; format[i] != '\0'; i++)
-	{
-		if (format[i] != '%')
-			count += _putchar(format[i]);
+    va_start(ap, format);
 
-		/* we jump in here because format is a % */
-		else
-		{
-			if (format[i + 1] == '%')
-			{
-				print_mod();
-				count++;
-				i++;
-			}
+    if (!format || strcmp(format, "%") == 0)
+        return (-1);
 
-			else
-			{
-				for (j = 0; ops[j].spec != '\0'; j++)
-				{
-					if (format[i + 1] == ops[j].spec)
-					{
-						count += ops[j].func(ap);
-						i++;
-						specf = 1;
-						break;
-					}
-				/* next char spec == null no match found call print_mod() */
-				}
+    for (i = 0; format[i] != '\0'; i++)
+    {
+        if (format[i] != '%')
+        {
+            count += _putchar(format[i]);
+        }
+        else
+        {
+            if (format[i + 1] == '%')
+            {
+                count += _putchar('%');
+                i++;
+            }
+            else
+            {
+                for (j = 0; ops[j].spec != '\0'; j++)
+                {
+                    if (format[i + 1] == ops[j].spec)
+                    {
+                        count += ops[j].func(ap);
+                        specf = 1;
+                        i++;
+                        break;
+                    }
+                }
 
-				if (!specf)
-				{
-					count += _putchar('%') + _putchar(format[i + 1]);
-					i++;
-				}
-			}
-		}
-	}
-	va_end(ap);
-	return (count);
+                if (!specf)
+                {
+                    count += _putchar('%') + _putchar(format[i + 1]);
+                    i++;
+                }
+            }
+        }
+    }
+
+    va_end(ap);
+    return (count);
 }
