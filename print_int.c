4196d7116259700120c9d84b85894d3d1a7ddee4
vincentcheri <52957917+vincentcheri@users.noreply.github.com>
2023-11-18 16:56:09 +0100

    Update print_int.c

--- print_int.c
+++ print_int.c
@@ -10,40 +10,26 @@
 
 int print_int(va_list ap)
 {
-	int digits = 1, divider = 1;
-	int n = va_arg(ap, int);
-	int num = n, j = 0, j2 = 0, neg = 0;
+	int a, expo = 1, len = 0;
+	unsigned int n;
 
-	if (n == 0)
+	a = va_arg(integ, int);
+
+	if (a < 0)
 	{
-		_putchar('0');
-		return (0);
+		len = len + _putchar('-');
+		n = a * -1;
 	}
-	/* counts number of digits by increasing count and dividing by 10 */
-	while ((num / 10) != 0)
+	else
+		n = a;
+	while (n / expo > 9)
+		expo *= 10;
+
+	while (expo != 0)
 	{
-		digits++;
-		num /= 10;
+		len = len + _putchar(n / expo + '0');
+		n = n % expo;
+		expo = expo / 10;
 	}
-	/* loops through number from last digit to largest digit */
-	for (j = (digits - 1); j >= 0; j--)
-	{
-		divider = 1;
-		for (j2 = 0; j2 < j; j2++)
-			divider = divider * 10;
-		if (n < 0)
-		{
-			_putchar('-');
-			_putchar((0 - (n / divider)) + '0');
-			n = n % divider;
-			n = 0 - n;
-			neg = 1;
-		}
-		else
-		{
-			_putchar((n / divider) + '0');
-			n = n % divider;
-		}
-	}
-	return (digits - 1 + neg);
+	return (len);
 }
