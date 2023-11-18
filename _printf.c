dff17cbbc6e94ccefd390b7d91a5f7d4c3f74aa9
vincentcheri <52957917+vincentcheri@users.noreply.github.com>
2023-11-18 10:40:11 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -15,7 +15,7 @@ int _printf(const char *format, ...)
 	count = 0;
 	va_start(ap, format);
 
-	if (!format && strcmp(format, "%") == 0)
+	if (!format || strcmp(format, "%") == 0)
 		return (-1);
 
 	for (i = 0; format[i] != '\0'; i++)
