53f2bb3c14254726b86e6748d875c28050a59158
vincentcheri <52957917+vincentcheri@users.noreply.github.com>
2023-11-18 17:04:35 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,49 +1,67 @@
 #include "main.h"
+#include <string.h>
 
 /**
- * _printf - A light-weight implementation of standard printf function.
- * @format: pointer to a string
- * Return: number of char printed
- */
+* _printf - A light-weight implementation of standard printf function.
+* @format: Pointer to a constant string.
+* Return: Number of characters been printed.
+*/
 
 int _printf(const char *format, ...)
 {
-	int count, i;
-	va_list ap;
+map_t ops[] = {
+{'c', print_char}, {'s', print_str}, {'\0', NULL}};
 
-	count = 0;
-	va_start(ap, format);
-	if (!format || strcmp(format, "%") == 0)
-		return (-1);
-	for (i = 0; format[i] != '\0'; i++)
-	{
-		if (format[i] != '%')
-		{
-			count += _putchar(format[i]);
-		}
-		else
-		{
-			if (format[i + 1] == '%')
-			{
-				count += _putchar('%');
-				i++;
-			}
-			else
-			{
-				printf_function func = get_specifier_function(format[i + 1]);
-				if (func.spec != '\0')
-				{
-					count += func.func(ap);
-					i++;
-				}
-				else
-				{
-					count += _putchar('%') + _putchar(format[i + 1]);
-					i++;
-				}
-			}
-		}
-	}
-	va_end(ap);
-	return (count);
+int i, j, count = 0;
+va_list ap;
+int specf;
+
+specf = 0;
+va_start(ap, format);
+/* If The format passed is NULL in any form just exit with -1 */
+if (!format || strcmp(format, "%") == 0)
+return (-1);
+/* The main loop that replaces specifiers with their corrs args */
+for (i = 0; format[i] != '\0'; i++)
+{
+if (format[i] != '%')
+{
+count += _putchar(format[i]);
+}
+
+/* we jump in here because format is a % */
+else
+{
+if (format[i + 1] == '%')
+{
+_putchar('%');
+count++;
+i++;
+}
+
+else
+{
+for (j = 0; ops[j].spec != '\0'; j++)
+{
+if (format[i + 1] == ops[j].spec)
+{
+count += ops[j].func(ap);
+i++;
+specf = 1;
+break;
+}
+}
+
+if (!specf)
+{
+_putchar('%');
+_putchar(format[i + 1]);
+count += 2;
+i++;
+}
+}
+}
+}
+va_end(ap);
+return (count);
 }
