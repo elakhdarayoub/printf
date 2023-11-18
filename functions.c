4b7bd161a822a443123f16dd0382b52713aa6058
vincentcheri <52957917+vincentcheri@users.noreply.github.com>
2023-11-18 17:47:38 +0100

    Update functions.c

--- functions.c
+++ functions.c
@@ -49,13 +49,3 @@ int print_mod(void)
 {
 return (_putchar('%'));
 }
-
-/**
-* print_dec - Prints decimal.
-* @ap: The decimal to print.
-* Return: length of digits.
-*int print_dec(va_list ap)
-*{
-*int num = va_arg(ap, int);
-*}
-*/
