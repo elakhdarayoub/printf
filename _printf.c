63c1b2ca7275ee54862f60da7fad16d0b14a0d7e
vincentcheri <52957917+vincentcheri@users.noreply.github.com>
2023-11-19 13:28:46 +0100

    ternary method didnt work

--- _printf.c
+++ _printf.c
@@ -9,7 +9,6 @@ int _printf(const char *format, ...)
 {
 	int i, count = 0;
 	va_list ap;
-
 	va_start(ap, format);
 	if (!format || strcmp(format, "%") == 0)
 		return (-1);
@@ -31,13 +30,20 @@ int _printf(const char *format, ...)
 			else
 			{
 				map_t spec = get_specifier_function(format[i + 1]);
-
-				count += (spec.func != NULL)
-					? spec.func(ap)
-					: (_putchar('%') + _putchar(format[i + 1]) + (++i, 0));
+				if (spec.func != NULL)
+				{
+					count += spec.func(ap);
+					i++;
+				}
+				else
+				{
+					_putchar('%');
+					_putchar(format[i + 1]);
+					count += 2;
+					i++;
+				}
 			}
 		}
 	}
 	va_end(ap);
 	return (count);
-}
